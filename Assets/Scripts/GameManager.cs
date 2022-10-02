71818648dee8a1f01057cbd2621ff028421a6391
Saphirako <12539189+saphirako@users.noreply.github.com>
2022-10-02 14:40:47 -0400

    [WIP] Start gamestate logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour {
     // Declare singleton
     public static GameManager instance;
+    public enum GameState { Menu, Starting, Playing, GameOver };
+    public GameState State { get; private set; }
+    private PlayerInput input;
+    private PlayerSpawner spawner;
+
     public float ForwardMomentum;
     public float Score { get; private set; }
     public float GamePhase { get; private set; }
@@ -13,10 +19,40 @@ public class GameManager : MonoBehaviour {
             return;
         }
         else instance = this;
+
+        input = new PlayerInput();
+        instance.State = GameState.Menu;
+        spawner = GetComponentInChildren<PlayerSpawner>();
+
         DontDestroyOnLoad(this);
     }
 
+    void Start() {
+        GamePhase = 1;
+
+        input.UI.Click.performed += StartGame;
+        ToggleClickAction(true);
+    }
     public void IncrementScore(int deltaPoints) {
         Score += deltaPoints;
+    }
+    public void PlayGame() {
+        instance.State = GameState.Playing;
+    }
+
+    private void ToggleClickAction(bool turnOn) {
+        if (turnOn) input.UI.Click.Enable(); else input.UI.Click.Disable();
+    }
+    private void StartGame(InputAction.CallbackContext context) {
+        if (instance.State == GameState.Menu) {
+            var mousePosition = Input.mousePosition;
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, mousePosition - Camera.main.ScreenToWorldPoint(mousePosition), Mathf.Infinity);
+
+            // If we hit nothing or we hit one of the boundary/triggers, start the game and turn off UI clicking
+            if (!hit || hit.collider.tag == "Boundary") {
+                ToggleClickAction(false);
+                spawner.Spawn();
+            }
+        }
     }
 }
